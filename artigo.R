a38ce3a809238c33e42cfd806798f75e6efdfdb3
Bartô Alheiros <alheirosb@gmail.com>
2023-02-08 22:04:35 -0300

    2012_2022 hiv pernambuco

--- artigo.R
+++ artigo.R
@@ -4,6 +4,7 @@ if (!require(dplyr))
   install.packages("dplyr")
 
 library(dplyr)
+library(readr)
 
 
 ############### Carregando o banco de dados ###############
@@ -16,6 +17,7 @@ setwd("H:/Meu Drive/UFRPE/TCC/artigo/artigo")
 # Passo 2: carregar o banco de dados
 df <- read.csv('dados-pernambuco-tuberculose-hiv-2012-2022.csv', sep = ';', dec = ',')
 df <- data.frame(df)
+View(df)
 
 #pega a quantidade de casos com hiv positivo no ano de 2022
 hiv_positivo_2022 <- df[df$HIV == 'Positivo', c('X2012','X2013','X2014','X2015','X2016','X2017','X2018','X2019','X2020','X2021','X2022')]
@@ -32,3 +34,11 @@ write.csv2(hiv_positivo_2022_novo, "2012-2022-hiv-tb.csv", row.names=TRUE)
 
 View(hiv_positivo_2022_novo)
 View(hiv_positivo_2022)
+
+X2012_2022_hiv_tb <- read_delim("2012-2022-hiv-tb.csv", 
+                                delim = ";", escape_double = FALSE, trim_ws = TRUE)
+df_12_22 <- data.frame(X2012_2022_hiv_tb)
+View(df_12_22)
+
+df_12_22_nova = df_12_22 %>% mutate('...1' = NULL)
+head(df_12_22_nova)
